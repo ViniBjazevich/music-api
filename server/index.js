a571cc40b89ca5bd6136d00fba2ddc3758cf2bd9
Vini Bjazevich <vinibjazevich97@gmail.com>
2022-01-23 19:07:06 -0600

    Added post to insert albums

--- server/index.js
+++ server/index.js
@@ -2,19 +2,29 @@ const express = require('express')
 const db = require('../database/index')
 const app = express()
 const cors = require('cors')
+const { response } = require('express')
 const port = 5000
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
-
-app.get('/db', (req, res) => {
   db
     .query("SELECT * FROM project")
-    .then(response => res.send(response.rows))
+    .then(data => res.send(data.rows))
+    .catch(e => {
+      console.error(e.stack)
+      res.send(e.stack)
+    })
+})
+
+app.post('/', (req, res) => {
+  const {name, artist, riaa, first_week_sales, release_date, streams, genre, project_type} = req.body;
+
+  db
+    .query(`INSERT INTO project (name, artist, riaa, first_week_sales, streams, release_date, genre, project_type)
+    VALUES ('${name}', '${artist}', ${riaa}, ${first_week_sales}, ${streams}, ${release_date}, '${genre}', '${project_type}')`)
+    .then(data => res.send(`${name} by ${artist} was successfully added to the database.`))
     .catch(e => {
       console.error(e.stack)
       res.send(e.stack)
