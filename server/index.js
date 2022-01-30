651cd5243fddf6909b8639eecee4a17263cbc940
Vini Bjazevich <vinibjazevich97@gmail.com>
2022-01-29 21:37:38 -0600

    changed delete to use id

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
+  const query = `SELECT * FROM project`
+
   db
-    .query("SELECT * FROM project")
+    .query(query)
     .then(data => res.send(data.rows))
     .catch(e => {
       console.error(e.stack)
@@ -18,14 +20,34 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/', (req, res) => {
-  let {name, artist, riaa, first_week_sales, release_date, streams, genre, project_type} = req.body;
+app.get('/:name/:artist', (req, res) => {
+  let {name, artist} = req.params;
+
   name = name.replace(/\'/g,`''`)
   artist = artist.replace(/\'/g,`''`)
 
   db
-    .query(`INSERT INTO project (name, artist, riaa, first_week_sales, streams, release_date, genre, project_type)
-    VALUES ('${name}', '${artist}', ${riaa}, ${first_week_sales}, ${streams}, ${release_date}, '${genre}', '${project_type}')`)
+    .query(`SELECT * FROM project WHERE artist = '${artist}' AND name = '${name}'`)
+    .then(data => res.send(data.rows))
+    .catch(e => {
+      console.error(e.stack)
+      res.send(e.stack)
+    })
+})
+
+
+app.post('/', (req, res) => {
+  let {name, artist, riaa, first_week_sales, release_date, streams, genre, project_type} = req.body;
+
+  name = name.replace(/\'/g,`''`)
+  artist = artist.replace(/\'/g,`''`)
+
+  const query = `INSERT INTO project
+    (name, artist, riaa, first_week_sales, streams, release_date, genre, project_type)
+    VALUES ('${name}', '${artist}', ${riaa}, ${first_week_sales}, ${streams}, ${release_date}, '${genre}', '${project_type}')`
+
+  db
+    .query(query)
     .then(data => res.send(`${name} by ${artist} was successfully added to the database.`))
     .catch(e => {
       console.error(e.stack)
@@ -33,15 +55,55 @@ app.post('/', (req, res) => {
     })
 })
 
-app.delete('/:name/:artist', (req, res) => {
-  let {name, artist} = req.params;
+app.put('/', (req, res) => {
+  let {name, artist, riaa, first_week_sales, release_date, streams, genre, project_type} = req.body;
 
   name = name.replace(/\'/g,`''`)
   artist = artist.replace(/\'/g,`''`)
 
+  const query = `UPDATE project
+                  SET
+                    name = '${name}',
+                    artist = '${artist}',
+                    riaa = '${riaa}',
+                    first_week_sales = '${first_week_sales}',
+                    streams = '${streams}',
+                    release_date = '${release_date}',
+                    genre = '${genre}',
+                    project_type = '${project_type}'
+                  WHERE
+                    artist = '${artist}' AND name = '${name}'`
+
   db
-    .query(`DELETE FROM project WHERE artist = '${artist}' AND name = '${name}'`)
-    .then(data => res.send(`${name} by ${artist} was successfully deleted from the database.`))
+    .query(query)
+    .then(data => res.send(`${name} by ${artist} was successfully updated in the database.`))
+    .catch(e => {
+      console.error(e.stack)
+      res.send(e.stack)
+    })
+})
+
+// app.delete('/:name/:artist', (req, res) => {
+//   let {name, artist} = req.params;
+
+//   name = name.replace(/\'/g,`''`)
+//   artist = artist.replace(/\'/g,`''`)
+
+//   db
+//     .query(`DELETE FROM project WHERE artist = '${artist}' AND name = '${name}'`)
+//     .then(data => res.send(`${name} by ${artist} was successfully deleted from the database.`))
+//     .catch(e => {
+//       console.error(e.stack)
+//       res.send(e.stack)
+//     })
+// })
+
+app.delete('/:id', (req, res) => {
+  let {id} = req.params;
+
+  db
+    .query(`DELETE FROM project WHERE id = ${id}`)
+    .then(data => res.send(`Project successfully deleted from the database.`))
     .catch(e => {
       console.error(e.stack)
       res.send(e.stack)
